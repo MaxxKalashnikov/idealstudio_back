10b55e23c9337fd4853f8e7ac730b492f927e361
Gharib Hussain <gharibhussain52@gmail.com>
2024-04-04 18:03:49 +0300

    Added index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// starts the server application
+// use environment variables
+require('dotenv').config()
+//console.log(process.env)
+const express = require('express')
+const cors = require('cors')
+
+
+// app
+const app = express()
+app.use(cors())
+app.use(express.json())
+// define that express can read parameters from url address
+app.use(express.urlencoded({extended: false}))
+
+
+
+// routers
+
+
+
+
+
+
+const port = process.env.PORT 
+app.listen(port)
