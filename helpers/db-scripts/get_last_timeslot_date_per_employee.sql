f0e285288f925680e80becb2ca15e8d237353bd8
Gharib Hussain <gharibhussain52@gmail.com>
2024-04-05 07:05:22 +0300

    Modified DB scripts.

--- helpers/db-scripts/get_last_timeslot_date_per_employee.sql
+++ helpers/db-scripts/get_last_timeslot_date_per_employee.sql
@@ -1,25 +1,31 @@
-
 CREATE OR REPLACE FUNCTION GetLastTimeslotDatePerEmployee()
 RETURNS TABLE (
+    timeslot_id INT,
     employee_id INT,
     firstname VARCHAR,
     lastname VARCHAR,
-    timeslot_date DATE
+    last_timeslot_date DATE
 ) AS $$
 BEGIN
     RETURN QUERY
     SELECT 
+        t.timeslot_id,
         t.employee_id,
         e.firstname,
         e.lastname,
-        MAX(t.timeslot_date) AS last_timeslot_date
+        t.last_timeslot_date
     FROM 
-        timeslot t
+        (
+            SELECT 
+    			timeslot.employee_id,
+    			MAX(timeslot.timeslot_id) as timeslot_id,
+    			MAX(timeslot.timeslot_date) as last_timeslot_date
+			FROM 
+    		timeslot 
+	GROUP BY 
+    	timeslot.employee_id
+		) t
     INNER JOIN 
-        employee e ON t.employee_id = e.employee_id
-    GROUP BY 
-        t.employee_id, 
-        e.firstname, 
-        e.lastname;
+        employee e ON t.employee_id = e.employee_id;
 END;
 $$ LANGUAGE plpgsql;
