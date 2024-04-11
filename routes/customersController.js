8c2de0345af494b9526a51b58752288d9c041295
MaxxKalashnikov <max.vanholl@mail.ru>
2024-04-11 10:26:20 +0300

    Fixed customer router

--- routes/customersController.js
+++ routes/customersController.js
@@ -5,7 +5,7 @@ const customersRouter = express.Router()
 // get all customers
 customersRouter.get('/', async (req, res) => {
     try {
-        const result = await query('select * from customers')
+        const result = await query('select * from customer')
         // in case the result may have no rows
         const rows = result.rows ? result.rows : []
         res.status(200).json(rows)
@@ -19,7 +19,7 @@ customersRouter.get('/', async (req, res) => {
 // get customer by id
 customersRouter.get('/:customer_id', async (req, res) => {
     try {
-        const result = await query('select * from customers where customer_id=($1)', 
+        const result = await query('select * from customer where customer_id=($1)', 
         [req.params.customer_id])
         // in case the result may have no rows
         const rows = result.rows ? result.rows : []
@@ -34,7 +34,7 @@ customersRouter.get('/:customer_id', async (req, res) => {
 // delete a customer
 customersRouter.delete('/delete/:customer_id', async (req, res) => {
     try {
-        const result = await query('delete from customers where customer_id=($1)', 
+        const result = await query('delete from customer where customer_id=($1)', 
         [req.params.customer_id])
         // in case the result may have no rows
         const rows = result.rows ? result.rows : []
@@ -46,7 +46,7 @@ customersRouter.delete('/delete/:customer_id', async (req, res) => {
     }
 })
 
-// add new employee
+// add new customer
 customersRouter.post('/new', async (req, res) => {
     try {
         const firstname = req.body.firstname
@@ -58,6 +58,7 @@ customersRouter.post('/new', async (req, res) => {
         [firstname, lastname, email, phone])
 
         const rows = result.rows ? result.rows : []
+        console.log(rows)
         res.status(200).json(rows)
     } catch (error) {
         console.log(error)
